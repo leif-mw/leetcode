75ae8571b37cc23c5b515edd780059126b11f15f
Leif Martens-Wallace <leif.martens-wallace@mail.mcgill.ca>
2022-03-09 04:36:26 -0500

    Time: 0 ms (100.00%), Space: 39.3 MB (60.73%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -5,5 +5,4 @@ class Solution
         // Uses the properties of binary representations of powers of two to check whether the given integer is a power of two.
         return (n & (n - 1)) == 0 && n > 0; 
     }
-
 }
