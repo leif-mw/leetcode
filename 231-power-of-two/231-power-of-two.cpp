8c61b82b4b5537b7b5ec556f9ad3cf3a55193ee6
Leif Martens-Wallace <leif.martens-wallace@mail.mcgill.ca>
2022-03-09 12:20:51 -0500

    Time: 0 ms (100.00%), Space: 5.9 MB (26.60%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution 
+{
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        // Uses the properties of binary representations of powers of 2 to check whether the given integer is a power of two.
+        return n > 0 && not (n & n - 1);
+    }
+};
