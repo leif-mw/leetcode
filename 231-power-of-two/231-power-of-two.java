4b15c9280224b3474be41876e222493a6651c7e6
Leif Martens-Wallace <leif.martens-wallace@mail.mcgill.ca>
2022-03-09 01:05:02 -0500

    Update 231-power-of-two.java

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -2,7 +2,7 @@ class Solution
 {
     public boolean isPowerOfTwo(int n) 
     {
-        // Uses the properties of binary representations of powers of 2 to check whether the given integer is a power of two.
+        // Uses the properties of binary representations of powers of two to check whether the given integer is a power of two.
         return (n & (n - 1)) == 0 && n > 0; 
     }
 
