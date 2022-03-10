cccdd2c5abc023321d43b4412b3b04067fea6c1b
Leif Martens-Wallace <leif.martens-wallace@mail.mcgill.ca>
2022-03-09 20:39:08 -0500

    Delete 231-power-of-two.cpp

--- 231-power-of-two/231-power-of-two.cpp
+++
@@ -1,9 +0,0 @@
-class Solution 
-{
-public:
-    bool isPowerOfTwo(int n) 
-    {
-        // Uses the properties of binary representations of powers of 2 to check whether the given integer is a power of two.
-        return n > 0 && !(n & n - 1);
-    }
-};
