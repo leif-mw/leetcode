a83df49ee2595908936f97b5bf2c9de11ef7e361
Leif Martens-Wallace <leif.martens-wallace@mail.mcgill.ca>
2022-08-07 12:53:07 -0400

    Time: 22 ms (16.85%), Space: 12.9 MB (39.40%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int length = nums.size();
+        for(int i = 0; i < length; i++)
+            nums.push_back(nums[i]);
+        return nums;
+    }
+};
